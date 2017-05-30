bfdb97b18de21053dcecb72a82f1e76867af2a72
ubuntu <b.vollmer@student.uni-tuebingen.de>
2017-05-30 23:36:06 +0200

    cassandra test script extended with keyspace selection

--- cassandra_test.py
+++ cassandra_test.py
@@ -1,17 +1,35 @@
+#!/usr/bin/env python
+
 from cassandra.cluster import Cluster
-from cassandra.policies import DCAwareRoundRobinPolicy
+# from cassandra.policies import DCAwareRoundRobinPolicy
 
-cluster = Cluster(
-        ['172.17.0.3', '172.17.0.2'],
-        load_balancing_policy=DCAwareRoundRobinPolicy(local_dc='US_EAST'),
-        port=9042)
+# constants
+KEY_SPACE='keyspace_sdn'
+
+# CONNECT TO SESSION
+
+# cluster = Cluster(
+#         ['172.17.0.3', '172.17.0.2'],
+#         load_balancing_policy=DCAwareRoundRobinPolicy(local_dc='US_EAST'),
+#         port=9042)
+
+cluster = Cluster(['172.17.0.3', '172.17.0.2'])
 session = cluster.connect()
-session.set_keyspace('keyspace_sdn')
 
-session.execute(
-        """
-        INSERT INTO switches (name, IP)
-        VALUES (%s, %s)
-        """,
-        ("John O'Reilly", "192.168.0.1")
-)
+# session.execute(
+#         """
+#         INSERT INTO switches (name, IP)
+#         VALUES (%s, %s)
+#         """,
+#         ("John O'Reilly", "192.168.0.1")
+# )
+
+keyspaces = cluster.metadata.keyspaces
+if KEY_SPACE in keyspaces:
+    print ('Keyspace found {0}').format(KEY_SPACE)
+else:
+    query_create_keyspace ='CREATE KEYSPACE {} WITH REPLICATION = {{ {} : {}, {} : {} }};'.format(KEY_SPACE,"'class'","'SimpleStrategy'","'replication_factor'",3)
+    print (query_create_keyspace)
+    session.execute(query_create_keyspace)
+
+session.set_keyspace(KEY_SPACE)
--- create_cluster_simple.sh
+++ create_cluster_simple.sh
@@ -1,5 +1,23 @@
 TAG='latest'
+name_app1="some-cassandra"
+name_app2="some-cassandra2"
+# Ports to publish
+PUB_PORT='9042'
+
+
+if docker ps | awk -v name_app1="name_app1" 'NR>1{  ($(NF) == name_app1 )  }'; then
+    docker stop "$name_app1"
+    docker rm -f "$name_app1"
+fi
+if docker ps | awk -v name_app2="name_app2" 'NR>1{  ($(NF) == name_app2 )  }'; then
+    docker stop "$name_app2"
+    docker rm -f "$name_app2"
+fi
+
 echo "Starting container 1"
-docker run --name some-cassandra -d cassandra:$TAG
+docker run --name $name_app1 -p $PUB_PORT -d cassandra:$TAG
+echo "sleeping ..."
+sleep 3
 echo "Starting container 2"
-docker run --name some-cassandra2 -d --link some-cassandra:cassandra cassandra:$TAG
+#docker run --name $name_app2 -d --link $name_app1:cassandra cassandra:$TAG
+docker run --name $name_app2 -p $PUB_PORT -d -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' $name_app1)" cassandra:$TAG
