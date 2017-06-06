f4e51f8e83ed2f703272b79c2fde3005603c1cd0
Benny93 <vollmerbenjamin93@gmail.com>
2017-06-06 20:53:43 +0200

    Fixed connection bug

--- testbed1/create_cassandra_container.sh
+++ testbed1/create_cassandra_container.sh
@@ -21,7 +21,7 @@ case $(hostname -s) in
          CONTAINER_NAME="cassandra2"
          IP='10.0.0.2'
          CLUSTER_PARTNER_IP='10.0.0.1'
-         ENV_VARS="-e CASSANDRA_BROADCAST_ADDRESS="${IP}" -e CASSANDRA_SEEDS="${CLUSTER_PARTNER_IP}
+         ENV_VARS='-e CASSANDRA_BROADCAST_ADDRESS='${IP}' -e CASSANDRA_SEEDS='${CLUSTER_PARTNER_IP}
      ;;
 esac
 
@@ -35,4 +35,5 @@ if docker ps | awk 'NR>1{  ($(NF) == '${CONTAINER_NAME}' )  }'; then
 fi
 
 echo "Starting container "${CONTAINER_NAME}
-docker run --name ${CONTAINER_NAME} -d ${ENV_VARS} -p ${IP}:${CQL_PORT} -p ${IP}:${INTER_NODE_PORT} cassandra:${TAG}
+#echo "docker run --name "${CONTAINER_NAME}" -d "${ENV_VARS}" -p "${IP}"::"${CQL_PORT}" -p "${IP}"::"${INTER_NODE_PORT}" cassandra:"${TAG}
+docker run --name ${CONTAINER_NAME} -d ${ENV_VARS} -p ${CQL_PORT}:${CQL_PORT} -p ${INTER_NODE_PORT}:${INTER_NODE_PORT} cassandra:${TAG}
