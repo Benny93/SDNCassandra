0c82c018cf3822f51f55146e144581bea13b84d1
Benny93 <vollmerbenjamin93@gmail.com>
2017-06-06 22:21:32 +0200

    Removed empty run script

--- testbed1/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# start scripts for ether controller1 or controller2
- #case $(hostname -s) in
- #    *1) ./create_cassandra_container_C1.sh;;
- #    *2) create_cassandra_container.sh;;
- #esac
-
