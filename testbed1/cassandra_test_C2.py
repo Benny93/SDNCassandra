a63095991758e1b8722b4044047d6bbcc7e92cf4
Benny93 <vollmerbenjamin93@gmail.com>
2017-06-06 22:16:49 +0200

    Got physically distributed cassandra instances running

--- testbed1/cassandra_test_C2.py
+++ testbed1/cassandra_test_C2.py
@@ -3,8 +3,12 @@ import sys
 import time
 from cassandra.cluster import Cluster
 import util_functions
+import random
 
 # constants
+# math
+M = 16 ** 4
+# network
 KEY_SPACE = 'keyspace_sdn'
 SWITCH_TABLE_NAME = 'switches'
 SWITCH_TABLE_ATTR1_NAME = 'ip_addr'
@@ -12,7 +16,7 @@ SWITCH_TABLE_ATTR2_NAME = 'controller_ip'
 SWITCH_TABLE_ATTR3_NAME = 'table_name'
 
 # CONNECT TO SESSION
-cluster = Cluster(['10.0.0.2'])
+cluster = Cluster(['10.0.0.1', '10.0.0.2'])
 session = cluster.connect()
 
 # Special KEYSPACE query
@@ -38,7 +42,6 @@ query_create_table_switch = session.prepare(
     SWITCH_TABLE_ATTR3_NAME + " VARCHAR);")
 query_delete_table_switches = session.prepare("DROP TABLE " + SWITCH_TABLE_NAME + ";")
 
-
 # manage_table_switches():
 # Create table switches, delete if existing
 ks = keyspaces.get(KEY_SPACE)
@@ -46,7 +49,7 @@ table = ks.tables.get(SWITCH_TABLE_NAME)
 
 if table:
     # clear table
-    print "Table "+SWITCH_TABLE_NAME+" already exists"
+    print "Table " + SWITCH_TABLE_NAME + " already exists"
     session.execute(query_delete_table_switches)
 session.execute(query_create_table_switch)
 
@@ -58,26 +61,22 @@ query_insert_into_switches = session.prepare(
     "VALUES (?, ?, ?);")
 query_select_all_from_switches = session.prepare("SELECT * FROM " + SWITCH_TABLE_NAME + ";")
 
-
 # insert DATA
 session.execute(query_insert_into_switches, ('2017:db8::f102', '2017:db8::f201', 'f102_db'))
 session.execute(query_insert_into_switches, ('2017:db8::f103', '2017:db8::f201', 'f103_db'))
-# read out DATA
+
+# insert and read out DATA
 try:
     while True:
         print(str(query_select_all_from_switches))
         table_switches = session.execute(query_select_all_from_switches)
+        """Insert random into database"""
+
+        session.execute(query_insert_into_switches, (
+            "2017:db8:" + "%x" % random.randint(0, M), '2017:db8::f201', 'f103_db'))
         print util_functions.pprinttable(table_switches.current_rows)
-        """
-        for row in table_switches:
-            row_formatted = "{}:{}, {}:{}, {}:{}".format(SWITCH_TABLE_ATTR1_NAME,getattr(row,SWITCH_TABLE_ATTR1_NAME),
-                                                         SWITCH_TABLE_ATTR2_NAME, getattr(row, SWITCH_TABLE_ATTR2_NAME),
-                                                         SWITCH_TABLE_ATTR3_NAME, getattr(row, SWITCH_TABLE_ATTR3_NAME),
-                                                         )
-            print row_formatted
-        """
         print "Sleeping for 2 seconds"
         time.sleep(2)
 except KeyboardInterrupt:
     print "Warning: Caught KeyboardInterrupt"
-    #sys.exit(0)
+    # sys.exit(0)
