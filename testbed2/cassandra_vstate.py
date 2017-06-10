d8a6b07096850bc6b1f0f69c564e2df94e7f8ece
Benny93 <vollmerbenjamin93@gmail.com>
2017-06-11 01:01:24 +0200

    Finally got writing and reading with vstate working. Using cqlengine

---
+++ testbed2/cassandra_vstate.py
@@ -0,0 +1,131 @@
+import time
+from cassandra.cluster import Cluster
+from cassandra.cqlengine.query import LWTException, QueryException
+
+import util_functions
+from cassandra.cqlengine import columns
+from cassandra.cqlengine.models import Model
+from cassandra.cqlengine import connection
+from cassandra.cqlengine.management import sync_table, drop_table
+from cassandra.cqlengine.management import create_keyspace_simple
+import json, ast
+
+
+class MacToPortDB(Model):
+    """
+    Used to store dictionary mac_to_port
+    Example: {1234: {44: 7, 55: 6}}
+
+    """
+    # TODO: consider using UUID again
+    # id = columns.UUID(primary_key=True)
+    dpid = columns.Integer(primary_key=True)
+    port_by_src = columns.Map(columns.Text, columns.Text)
+
+
+def drop_dict():
+    """
+Drops dictionary
+    """
+    drop_table(MacToPortDB)
+
+
+class VState(object):
+    """
+Abstraction of cassandra db access. Represents global network state
+    """
+    # class constants
+    KEY_SPACE = 'keyspace_sdn'
+    TABLE_NAME = 'dict'
+
+    def __init__(self):
+        # CONNECT TO SESSION
+        connection.setup(['10.0.0.1', '10.0.0.2'], 'cqlengine')
+        manage_keyspace(self.KEY_SPACE, connection.cluster, connection.session)
+        self.model = MacToPortDB()
+        MacToPortDB.__keyspace__ = self.KEY_SPACE
+        # ...and create your CQL table
+        self.model.update()
+        sync_table(MacToPortDB)
+
+    def update_dict(self, dpid, dictionary):
+        """
+Saves dictionary in cassandra db
+        :param dictionary:
+        """
+        try:
+            self.model.objects(dpid=dpid).if_exists().update(port_by_src=dictionary)
+            # switches = self.model.if_exists().update(**dictionary)
+            # switches.save()
+        except LWTException as e:
+            print e.message
+            pass
+
+    def get_dict(self, dpid):
+        """
+Returns dictionary
+        """
+        # check if dict is available
+        try:
+            dict_res = dict(self.model.objects.get(dpid=dpid))
+            # TODO: Fix unicode problems. This is a quickfix
+            return ast.literal_eval(json.dumps(dict_res['port_by_src']))
+        #            return dict_res['port_by_src']
+        except QueryException as e:
+            # try to create table
+            try:
+                dict_res = self.model.if_not_exists().create(dpid=dpid, port_by_src={})
+                dict_res.save()
+                return dict_res['port_by_src']
+            except LWTException as e:
+                # handle failure case
+                print e.existing  # existing object
+        # return empty
+        return {}
+
+
+# End of class
+
+def manage_keyspace(key_space, cluster, session):
+    # Special KEYSPACE query
+    """
+Set sdn namespace to current namespace. If namespace does not exist -> create
+    :param key_space: Name of the keyspace
+    :param cluster: Cluster of this session
+    :param session: Session
+    """
+    query_create_keyspace = ('CREATE KEYSPACE {} WITH REPLICATION =' +
+                             ' {{ {} : {}, {} : {} }};').format(
+        key_space, "'class'", "'SimpleStrategy'",
+        "'replication_factor'", 3)
+    # Keyspaces
+    keyspaces = cluster.metadata.keyspaces
+    if key_space in keyspaces:
+        print 'Keyspace found {0}'.format(key_space)
+    else:
+        print(query_create_keyspace)
+        session.execute(query_create_keyspace)
+    session.set_keyspace(key_space)
+
+
+def prepare_queries(session, query_by_name, table_name):
+    # Queries for creation and drop
+    """
+
+    :param session:
+    :param query_by_name:
+    :param table_name:
+    """
+    query_by_name.setdefault('create_table', {})
+    query_by_name['create_table'] = session.prepare(
+        "CREATE TABLE " + table_name +
+        " (" + "dict" + " VARCHAR PRIMARY KEY);")
+    query_by_name.setdefault('delete_table', {})
+    query_by_name['delete_table'] = session.prepare("DROP TABLE " + table_name + ";")
+
+# Test and Debug area
+# mystate = VState()
+# res = mystate.get_dict(1)
+# res['a'] = 'b'
+# mystate.update_dict(1, res)
+# res = mystate.get_dict(1)
--- testbed2/simple_switch_13_vstate.py
+++ testbed2/simple_switch_13_vstate.py
@@ -1,7 +1,12 @@
 from ryu.app import simple_switch_13
+from ryu.controller import ofp_event
+from ryu.controller.handler import set_ev_cls, MAIN_DISPATCHER
+from ryu.lib.packet import ether_types
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import packet
 
 
-class SimpleSwitch13VState(simple_switch_13):
+class SimpleSwitch13VState(simple_switch_13.SimpleSwitch13):
     """
     This Class implements a simple learning switch, which stores its dictionaries
     inside a virtual state data base.
@@ -9,3 +14,58 @@ class SimpleSwitch13VState(simple_switch_13):
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13VState, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
