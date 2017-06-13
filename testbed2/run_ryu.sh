2b39c140485f8513c7031561b03543b035eb0c60
Benny93 <vollmerbenjamin93@gmail.com>
2017-06-13 14:06:39 +0200

    cleanup 2

--- testbed2/run_ryu.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-PATH_RYU_APP='~/sf_SDN/ryu/ryu/app/'
-OPTIONS=' --verbose --observe-links '
-APP_NAME='simple_switch_13.py'
-APP2_NAME='ws_topology.py'
-ryu-manager ${OPTIONS} "ryu.app.ws_topology"
